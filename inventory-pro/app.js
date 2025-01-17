9fb296be5a803065a4eb7bc288b0ce8d37b94b2b
Eddie Maceda <eddiemaceda11@gmail.com>
2025-01-16 19:19:50 -0700

    Create app.js file, set up server and import necessary modules

---
+++ inventory-pro/app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+
+require("dotenv").config();
+app.set("view engine", "ejs");
+app.use(express.urlencoded({extended: true}));
+
+const indexRouter = require("./routes/indexRouter");
+const propertiesRouter = require("./routes/propertiesRouter");
+const propertyRouter = require("./routes/propertyRouter");
+
+app.use("/", indexRouter);
+app.use("/properties", propertiesRouter);
+app.use("/property", propertyRouter);
+
+const PORT = 3300;
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}`);
+  console.log(process.env.PORT)
+})
+
+
+
+
+
+
+
+
