d4dbc35af903eff8f0c963614e616d33e773ff77
Eddie Maceda <eddiemaceda11@gmail.com>
2025-01-20 20:32:26 -0700

    Remove dummy data, and create function to query all properties from the DB

--- inventory-pro/controllers/indexController.js
+++ inventory-pro/controllers/indexController.js
@@ -1,25 +1,11 @@
-const properties = [
-  {
-    id: 1,
-    name: "Homes",
-  },
-  {
-    id: 2,
-    name: "Apartments",
-  },
-  {
-    id: 3,
-    name: "Land"
-  },
-  {
-    id: 4,
-    name: "Condos"
-  }
-]
+const db = require ("../db/queries");
 
-exports.getIndexPage = (req, res) => {
+exports.getIndexPage = async (req, res) => {
+  const propertyTypes = await db.getAllProperties();
+  console.log(propertyTypes);
+
   res.render("index", {
     title: "Choose a home type",
-    properties: properties
+    propertyTypes,
   })
 }
