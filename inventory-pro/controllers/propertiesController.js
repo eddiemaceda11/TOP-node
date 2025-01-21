6ab5be6c70901ed2c61f22943dc10ba50ed6cf0e
Eddie Maceda <eddiemaceda11@gmail.com>
2025-01-20 20:33:35 -0700

    Create function to query a specific property by home type from the DB

--- inventory-pro/controllers/propertiesController.js
+++ inventory-pro/controllers/propertiesController.js
@@ -1,4 +1,9 @@
-exports.getCategorizedProperties = (req, res) => {
+const db = require("../db/queries");
+
+exports.getCategorizedProperties = async (req, res) => {
+  const propertiesCategorized = await db.getPropertiesByHomeType(req.params.type);
+  console.log("Category: ", propertiesCategorized);
+
   res.render("properties", {
     title: "Properties"
   })
