3120f4a0ea09d08d1ad04143f6a5f328eac40361
Eddie Maceda <eddiemaceda11@gmail.com>
2025-01-20 18:44:37 -0700

    Create the pg pool

---
+++ inventory-pro/db/pool.js
@@ -0,0 +1,9 @@
+const { Pool } = require("pg");
+
+module.exports = new Pool({
+  host: "localhost",
+  user: process.env.HOSTNAME,
+  database: "inventory_pro",
+  password: process.env.PASSWORD,
+  port: 5432,
+})
