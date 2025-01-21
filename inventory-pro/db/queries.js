d4dcf038626f51ae0489860c757a541d5ce82da4
Eddie Maceda <eddiemaceda11@gmail.com>
2025-01-20 21:28:28 -0700

    Create query function to get a single property from the database

--- inventory-pro/db/queries.js
+++ inventory-pro/db/queries.js
@@ -1,16 +1,23 @@
 const pool = require("./pool");
 
 async function getAllProperties() {
-  const {rows} = await pool.query("SELECT * FROM home_type");
+  const { rows } = await pool.query("SELECT * FROM home_type");
   return rows;
 }
 
 async function getPropertiesByHomeType(type) {
-  const {rows} = await pool.query("SELECT * FROM property INNER JOIN home_type ON property.home_type_id = home_type.id WHERE home_type.type = ($1)", [type]);
+  const { rows } = await pool.query("SELECT * FROM property INNER JOIN home_type ON property.home_type_id = home_type.id WHERE home_type.type = ($1)", [type]);
+  return rows;
+}
+
+async function getPropertyById(id) {
+  const { rows } = await pool.query("SELECT * FROM property WHERE id = ($1", [id]);
+  console.log(rows);
   return rows;
 }
 
 module.exports = {
   getAllProperties,
   getPropertiesByHomeType,
-}
+  getPropertyById,
+};
