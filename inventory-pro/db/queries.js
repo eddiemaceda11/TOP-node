afe316f82980eb8d9b8fd9c1562108fa80948c43
Eddie Maceda <eddiemaceda11@gmail.com>
2025-01-23 20:40:28 -0700

    Create the getPropertiesHomeTypeByID and deletePropertyQuery query functions

--- inventory-pro/db/queries.js
+++ inventory-pro/db/queries.js
@@ -5,6 +5,11 @@ async function getAllProperties() {
   return rows;
 }
 
+async function getPropertiesHomeTypeByID(id) {
+  const { rows } = await pool.query("SELECT type FROM home_type WHERE id = ($1) LIMIT 1", [id]);
+  return rows;
+}
+
 async function getPropertiesByHomeType(type) {
   const { rows } = await pool.query("SELECT * FROM home_type INNER JOIN property ON property.home_type_id = home_type.id WHERE home_type.type = $1", [type]);
   return rows;
@@ -12,12 +17,19 @@ async function getPropertiesByHomeType(type) {
 
 async function getPropertyById(id) {
   const { rows } = await pool.query("SELECT * FROM property WHERE id = ($1)", [id]);
-  console.log(rows);
+  return rows;
+}
+
+async function deletePropertyQuery(id) {
+  const { rows } = await pool.query("DELETE FROM property WHERE id = ($1)", [id]);
+  console.log("Deleted rows: ", rows);
   return rows;
 }
 
 module.exports = {
   getAllProperties,
+  getPropertiesHomeTypeByID,
   getPropertiesByHomeType,
   getPropertyById,
+  deletePropertyQuery,
 };
