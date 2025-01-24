bd6cda82f0341a91c7937dc94cf362e00bd835ca
Eddie Maceda <eddiemaceda11@gmail.com>
2025-01-23 17:53:17 -0700

    Configure the propertyController getProperty function

--- inventory-pro/controllers/propertyController.js
+++ inventory-pro/controllers/propertyController.js
@@ -1,5 +1,11 @@
-exports.getProperty = (req, res) => {
+const db = require("../db/queries");
+
+exports.getProperty = async (req, res) => {
+  const { id } = req.params;
+  const property = await db.getPropertyById(id);
+  console.log("Id is: ", property);
+
   res.render("property", {
-    title: "Property"
-  })
-}
+    title: "Property",
+  });
+};
