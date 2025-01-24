1abe57720edcbb98d640553f75ec0f77b9a671d0
Eddie Maceda <eddiemaceda11@gmail.com>
2025-01-23 20:37:52 -0700

    Add a form on each property to allot the user to perform a DELETE request

--- inventory-pro/routes/propertyRouter.js
+++ inventory-pro/routes/propertyRouter.js
@@ -2,6 +2,7 @@ const { Router } = require("express");
 const propertyRouter = Router();
 const propertyController = require("../controllers/propertyController");
 
+propertyRouter.get("/:home_type_id/delete/:property_id", propertyController.deleteProperty);
 propertyRouter.get("/:id", propertyController.getProperty);
 
 module.exports = propertyRouter;
