4a0ac8070469162d17bfa4e0c3ac7a1827d194e8
Eddie Maceda <eddiemaceda11@gmail.com>
2025-01-23 20:39:26 -0700

    Create a deleteProperty function in the propertyController

--- inventory-pro/controllers/propertyController.js
+++ inventory-pro/controllers/propertyController.js
@@ -3,9 +3,17 @@ const db = require("../db/queries");
 exports.getProperty = async (req, res) => {
   const { id } = req.params;
   const property = await db.getPropertyById(id);
-  console.log("Id is: ", property);
 
   res.render("property", {
     title: "Property",
+    property,
   });
+};
+
+exports.deleteProperty = async (req, res) => {
+  const { home_type_id, property_id } = req.params;
+  await db.deletePropertyQuery(property_id);
+  const categorizedPropertiesID = await db.getPropertiesHomeTypeByID(home_type_id);
+  console.log("id: ", categorizedPropertiesID);
+  res.redirect(`/properties/${categorizedPropertiesID[0].type}`);
 };
