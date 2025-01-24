c434f0d108a2813363894df7247e909361b6e53c
Eddie Maceda <eddiemaceda11@gmail.com>
2025-01-23 17:52:21 -0700

    Rework the getPropertiesByHomeType and getPropertyById function queries

--- inventory-pro/db/queries.js
+++ inventory-pro/db/queries.js
@@ -6,12 +6,12 @@ async function getAllProperties() {
 }
 
 async function getPropertiesByHomeType(type) {
-  const { rows } = await pool.query("SELECT * FROM property INNER JOIN home_type ON property.home_type_id = home_type.id WHERE home_type.type = ($1)", [type]);
+  const { rows } = await pool.query("SELECT * FROM home_type INNER JOIN property ON property.home_type_id = home_type.id WHERE home_type.type = $1", [type]);
   return rows;
 }
 
 async function getPropertyById(id) {
-  const { rows } = await pool.query("SELECT * FROM property WHERE id = ($1", [id]);
+  const { rows } = await pool.query("SELECT * FROM property WHERE id = ($1)", [id]);
   console.log(rows);
   return rows;
 }
