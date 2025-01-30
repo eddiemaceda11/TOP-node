5d22ee339a6c3302d5d7aa4fb98c73014934ddb7
Eddie Maceda <eddiemaceda11@gmail.com>
2025-01-29 18:50:06 -0700

    Add temp comment notes about the serialize/deserialize functions

--- authentication/app.js
+++ authentication/app.js
@@ -77,3 +77,8 @@ app.post("/sign-up", async (req, res, next) => {
 app.listen(3000, () => {
   console.log("Server listening on port 3000");
 });
+
+//**NOTES FOR THE SERIALIZE + DESERIALIZE FUNCTIONS ABOVE**//
+// When a session is created, passport.serializeUser will receive the user object found from a successful login and store its id property in the session data. Upon some other request, if it finds a matching session for that request, passport.deserializeUser will retrieve the id we stored in the session data. We then use that id to query our database for the specified user, then done(null, user) attaches that user object to req.user. Now in the rest of the request, we have access to that user object via req.user.
+
+// Again, we aren’t going to be calling these functions on our own and we just need to define them, they’re used in the background by passport.
