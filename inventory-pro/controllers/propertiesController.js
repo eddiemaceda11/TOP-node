97e7674ea03e52c836b9179403bc7ac575d19ef0
Eddie Maceda <eddiemaceda11@gmail.com>
2025-02-06 20:36:38 -0700

    Remove unused comment

--- inventory-pro/controllers/propertiesController.js
+++ inventory-pro/controllers/propertiesController.js
@@ -2,7 +2,6 @@ const db = require("../db/queries");
 
 exports.getCategorizedProperties = async (req, res) => {
   const categorizedProperties = await db.getPropertiesByHomeType(req.params.type);
-  // console.log("Category: ", categorizedProperties);
 
   res.render("properties", {
     title: "Properties",
