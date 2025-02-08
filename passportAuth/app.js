3640cf76d44a4971c8d3a9f0940f87d7d319a138
Eddie Maceda <eddiemaceda11@gmail.com>
2025-02-08 12:00:41 -0700

    Create the setup for the pg-simple session

--- passportAuth/app.js
+++ passportAuth/app.js
@@ -6,6 +6,21 @@ const session = require("express-session");
 const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
 
+const session = require("express-session");
+
+// pg-simple session setup
+app.use(
+  session({
+    store: new (require("connect-pg-simple")(session))({
+      // Insert connect-pg-simple options here
+    }),
+    secret: process.env.FOO_COOKIE_SECRET,
+    resave: false,
+    cookie: { maxAge: 30 * 24 * 60 * 60 * 1000 }, // 30 days
+    // Insert express-session options here
+  })
+);
+
 const app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
