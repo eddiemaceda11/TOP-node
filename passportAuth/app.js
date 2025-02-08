ecb4fdae87818aed653bb7e8d68cf46543c0b20e
Eddie Maceda <eddiemaceda11@gmail.com>
2025-02-08 12:31:49 -0700

    Update the session configuration

--- passportAuth/app.js
+++ passportAuth/app.js
@@ -6,7 +6,8 @@ const session = require("express-session");
 const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
 
-const session = require("express-session");
+const app = express();
+app.use(express.urlencoded({ extended: true }));
 
 // pg-simple session setup
 app.use(
@@ -14,16 +15,19 @@ app.use(
     store: new (require("connect-pg-simple")(session))({
       // Insert connect-pg-simple options here
     }),
-    secret: process.env.FOO_COOKIE_SECRET,
+    secret: "cats",
     resave: false,
     cookie: { maxAge: 30 * 24 * 60 * 60 * 1000 }, // 30 days
     // Insert express-session options here
   })
 );
 
-const app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.send("Hello from sessions");
+});
 
 app.listen(3004, () => {
   console.log("Server running for passport prjoect");
