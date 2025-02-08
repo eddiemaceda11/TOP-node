cac4ea077bb5dad1ee5677103c43e2cc8de9be4f
Eddie Maceda <eddiemaceda11@gmail.com>
2025-02-08 12:53:02 -0700

    Reconfigure the pg-simple session configuration

--- passportAuth/app.js
+++ passportAuth/app.js
@@ -1,6 +1,7 @@
 const path = require("node:path");
 const bcrypt = require("bcryptjs");
 const { Pool } = require("pg");
+const pg = require("pg");
 const express = require("express");
 const session = require("express-session");
 const passport = require("passport");
@@ -10,12 +11,26 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 
 // pg-simple session setup
+const expressSession = require("express-session");
+const pgSession = require("connect-pg-simple")(expressSession);
+
+const pgPool = new pg.Pool({
+  host: "localhost",
+  user: "eddiemaceda",
+  database: "passport_sessions",
+  password: "test",
+  port: 5432,
+});
+
 app.use(
-  session({
-    store: new (require("connect-pg-simple")(session))({
+  expressSession({
+    store: new pgSession({
+      pool: pgPool, // Connection pool
+      tableName: "session_test", // Use another table-name than the default "session" one
       // Insert connect-pg-simple options here
     }),
     secret: "cats",
+    saveUninitialized: false,
     resave: false,
     cookie: { maxAge: 30 * 24 * 60 * 60 * 1000 }, // 30 days
     // Insert express-session options here
