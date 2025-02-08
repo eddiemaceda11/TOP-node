68ba72d2013aa77b3a3533f04938c336964213a6
Eddie Maceda <eddiemaceda11@gmail.com>
2025-02-08 11:47:24 -0700

    Create the initial setup of the app server

---
+++ passportAuth/app.js
@@ -0,0 +1,15 @@
+const path = require("node:path");
+const bcrypt = require("bcryptjs");
+const { Pool } = require("pg");
+const express = require("express");
+const session = require("express-session");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+
+const app = express();
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.listen(3004, () => {
+  console.log("Server running for passport prjoect");
+});
