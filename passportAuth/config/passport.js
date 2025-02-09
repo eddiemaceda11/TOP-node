62dc1a4b8aaab5dd702fe189ddef8e8c3cc22974
Eddie Maceda <eddiemaceda11@gmail.com>
2025-02-09 09:26:03 -0700

    Setup the initial configuration of the passport LocalStrategy

---
+++ passportAuth/config/passport.js
@@ -0,0 +1,16 @@
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const { pgPool } = require("../app");
+
+passport.use(
+  new LocalStrategy(async (username, password, done) => {
+    try {
+      const { rows } = await pgPool.query("SELECT * FROM users WHERE username = $1", [username]);
+      const user = rows[0];
+
+      if (!user) {
+        return done(null, false, { message: "Incorrect username" });
+      }
+    } catch (err) {}
+  })
+);
