a1986ff3d7a57c9f004a4f420c2675d8bcff673b
Eddie Maceda <eddiemaceda11@gmail.com>
2025-02-10 22:42:03 -0700

    Remove unused imports

--- passportAuth/app.js
+++ passportAuth/app.js
@@ -1,11 +1,9 @@
 const path = require("node:path");
 const bcrypt = require("bcryptjs");
-const { Pool } = require("pg");
+// const { Pool } = require("pg");
 const pg = require("pg");
 const express = require("express");
 const session = require("express-session");
-const passport = require("passport");
-const LocalStrategy = require("passport-local").Strategy;
 
 require("dotenv").config();
 
