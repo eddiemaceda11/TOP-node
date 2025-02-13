aa777771c5a0eae1fa02179a0783fdc3ba9ccba3
Eddie Maceda <eddiemaceda11@gmail.com>
2025-02-12 19:30:00 -0700

    Remove view engine

--- passportAuth/app.js
+++ passportAuth/app.js
@@ -50,8 +50,6 @@ app.use(
 app.use(express.urlencoded({ extended: true }));
 app.use(passport.session()); // gives us access to the req.sessions object
 
-app.set("view engine", "ejs");
-
 app.get("/", (req, res) => {
   res.send("Hello from sessions");
 });
