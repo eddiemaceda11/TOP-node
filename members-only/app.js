4546f9d6e88fee17a2131c2fd27e230828c71ec7
Eddie Maceda <eddiemaceda11@gmail.com>
2025-02-16 18:46:36 -0700

    Add a project overview

---
+++ members-only/app.js
@@ -0,0 +1,5 @@
+/**
+ PROJECT OVERVIEW: I will be building an exclusive clubhouse where members can write anonymous posts. Inside of the 
+                   clubhouse, members can see who the author of the post is, but outside they can only see the story
+                   and wonder who wrote it.
+ */
