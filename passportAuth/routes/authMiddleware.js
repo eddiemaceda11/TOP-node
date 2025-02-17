0acea5b5dbdd29b363241c4752b21bd6fe82e919
Eddie Maceda <eddiemaceda11@gmail.com>
2025-02-16 18:34:32 -0700

    Create the isAdmin middleware function

--- passportAuth/routes/authMiddleware.js
+++ passportAuth/routes/authMiddleware.js
@@ -6,3 +6,12 @@ module.exports.isAuth = (req, res, next) => {
     res.json({ message: "You are not authorized to view this message" });
   }
 };
+
+module.exports.isAdmin = (req, res, next) => {
+  if (req.isAuthenticated() && req.user.admin) {
+    console.log("Admin");
+    next();
+  } else {
+    res.json({ message: "You are not authorized to view this message" });
+  }
+};
