cb8949f82575f102d7034f60ab00988d3cf53aee
Eddie Maceda <eddiemaceda11@gmail.com>
2025-02-16 18:58:00 -0700

    Update the 'Steps' syntax

--- members-only/app.js
+++ members-only/app.js
@@ -3,10 +3,11 @@
                    clubhouse, members can see who the author of the post is, but outside they can only see the story
                    and wonder who wrote it.
  PROJECT PURPOSE:  The will incorporate the lessons of authentication, databases, and SQL covered to combine into one project.
- PROJECT STEPS:    1.)  Brainstorm around the database models that will be needed to accomplish this project. Requirements
+ PROJECT STEPS:    1)  Brainstorm around the database models that will be needed to accomplish this project. Requirements
                    include a fullname(first and last), username(email is fine for use), password and membership status.
                    Users need to be able to create message sthat have a title, a timestamp and some text. The database shoule
                    keep track of who created each message.
-                   2.)  Setup the database on PostgreSQL and generate or create the project skeleton, including the models
+                   2)  Setup the database on PostgreSQL and generate or create the project skeleton, including the models
                    designed in Step 1.
+                   3)  
  */
