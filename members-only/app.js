cb8b8cb1f5a644441029511fbb2fce0a75bae7c1
Eddie Maceda <eddiemaceda11@gmail.com>
2025-02-16 18:48:42 -0700

    Add a project purpose

--- members-only/app.js
+++ members-only/app.js
@@ -2,4 +2,5 @@
  PROJECT OVERVIEW: I will be building an exclusive clubhouse where members can write anonymous posts. Inside of the 
                    clubhouse, members can see who the author of the post is, but outside they can only see the story
                    and wonder who wrote it.
+ PROJECT PURPOSE:  The will incorporate the lessons of authentication, databases, and SQL covered to combine into one project.
  */
