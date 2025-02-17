96f09476cbd41163d23d3e19d1cbc08eedf3b9ab
Eddie Maceda <eddiemaceda11@gmail.com>
2025-02-16 18:07:16 -0700

    Add a temporary console.log()

--- passportAuth/routes/authMiddleware.js
+++ passportAuth/routes/authMiddleware.js
@@ -1,5 +1,6 @@
 module.exports.isAuth = (req, res, next) => {
   if (req.isAuthenticated()) {
+    console.log("Authenticated");
     next();
   } else {
     res.json({ message: "You are not authorized to view this message" });
