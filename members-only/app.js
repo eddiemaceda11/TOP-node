1a9486b4a5835d352db09503e5004846ca1f6cbc
Eddie Maceda <eddiemaceda11@gmail.com>
2025-02-16 19:51:37 -0700

    Add project Step 10

--- members-only/app.js
+++ members-only/app.js
@@ -16,4 +16,5 @@
                    7)  Display all member messages on the home page, but only show the author and date of the messages to other club-members.
                    8)  Add an optional field to the user model called Admin and then add the ability to delete messages, but only allow users who have admin == true to see the delete-button and delete messages. You’ll need to add a way to actually mark a user as an ‘admin’ so either add another secret pass-code page, or just put an “is admin” checkbox on the sign-up form.
                    9)  By this point, anyone who comes to the site should be able to see a list of all messages, with the author’s name hidden. Users should be able to sign-up and create messages, but ONLY users that are members should be able to see the author and date of each message. Finally, there should be an Admin user that is able to see everything and also has the ability to delete messages. 
+                   10)  Once complete. deploy the project on a chosen PaaS (list of PaaS providers from the Deployment lesson).
  */
