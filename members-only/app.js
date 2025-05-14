274f1189a1ab87eaa624cc3ab384026d1cbfcfcb
Eddie Maceda <eddiemaceda11@gmail.com>
2025-05-13 20:51:54 -0700

    Code refactor

--- members-only/app.js
+++ members-only/app.js
@@ -1,12 +1,10 @@
 /**
- PROJECT OVERVIEW: I will be building an exclusive clubhouse where members can write anonymous posts. Inside of the 
-                   clubhouse, members can see who the author of the post is, but outside they can only see the story
-                   and wonder who wrote it.
+ PROJECT OVERVIEW: I will be building an exclusive clubhouse where members can write anonymous posts. Inside of the clubhouse, members can see who the author of the post is, but outside they can only see the story and wonder who wrote it.
+
  PROJECT PURPOSE:  The will incorporate the lessons of authentication, databases, and SQL covered to combine into one project.
- PROJECT STEPS:    1)  Brainstorm around the database models that will be needed to accomplish this project. Requirements
-                   include a fullname(first and last), username(email is fine for use), password and membership status.
-                   Users need to be able to create messages that have a title, a timestamp and some text. The database should
-                   keep track of who created each message.
+
+ PROJECT STEPS:    1)  Brainstorm around the database models that will be needed to   
+                    accomplish this project. Requirements include a fullname(first and last), username(email is fine for use), password and membership status. Users need to be able to create messages that have a title, a timestamp and some text. The database should keep track of who created each message.
 
                    2)  Setup the database on PostgreSQL and generate or create the project skeleton, including the models
                    designed in Step 1.
@@ -28,29 +26,53 @@
                    10)  Once complete. deploy the project on a chosen PaaS (list of PaaS providers from the Deployment lesson).
  */
 // express-session pg passport passport-local
-const express = require("express");
+const express = require('express');
 const app = express();
-const router = require("./routes/routes");
-const pg = require("pg");
-const bcrypt = require("bcryptjs");
+const indexRouter = require('./routes/routes');
+// const pg = require('pg');
+const { pgPool } = require('./db/pool');
+const bcrypt = require('bcryptjs');
+const path = require('path');
+const LocalStrategy = require('passport-local').Strategy;
+const passport = require('passport');
 
 //pg-simple session setup
-const expressSession = require("express-session");
-const pgSession = require("connect-pg-simple")(expressSession); // in place of session
+const expressSession = require('express-session');
+const pgSession = require('connect-pg-simple')(expressSession); // in place of session
+require('dotenv').config();
+require('./config/passport-config');
+app.use(express.urlencoded({ extended: true }));
+app.use(passport.session()); // gives us access to the req.sessions object
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
-const passport = require("passport");
-
-require("dotenv").config();
-
-const pgPool = new pg.Pool({
-  host: "localhost",
-  user: process.env.USERNAME,
-  database: "members_only",
-  password: process.env.passport,
-  port: 5432,
+// check PostgreSQL connection
+pgPool.connect((err, client, release) => {
+  if (err) {
+    console.error('Error acquiring client', err.stack);
+  } else {
+    console.log('Connected to PostgresSQL session store!');
+    release();
+  }
 });
 
-app.use("/", router);
+// Setting up our express session w/ pg-simple
+app.use(
+  expressSession({
+    store: new pgSession({
+      pool: pgPool, // Connection pool,
+      tableName: 'pg_session', // Use another table-name than the default "session" one
+      // Insert connect-pg-simple options here
+    }),
+    secret: process.env.SECRET,
+    saveUninitialized: true,
+    resave: false,
+    cookie: { maxAge: 30 * 24 * 60 * 60 * 1000 }, // 30 days
+    // Insert express-session options here
+  })
+);
+
+app.use('/', indexRouter);
 
 app.listen(3005, () => {
   console.log("Now we're running on Server 3005");
