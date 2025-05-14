d0ac95f8ca3bb9905589c060a903dccf856dd054
Eddie Maceda <eddiemaceda11@gmail.com>
2025-05-13 18:58:35 -0700

    Create the pgPool

---
+++ members-only/db/pool.js
@@ -0,0 +1,9 @@
+const { pg } = require('pg');
+
+const pgPool = new pg.Pool({
+  host: 'localhost',
+  user: process.env.USERNAME,
+  database: 'members_only',
+  password: process.env.PASSWORD,
+  port: 5432,
+});
