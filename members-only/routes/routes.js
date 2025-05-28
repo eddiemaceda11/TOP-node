1583753cd4786cc5fd3ec18d28c9610b09595262
Eddie Maceda <eddiemaceda11@gmail.com>
2025-05-27 21:22:06 -0700

    Refactor routes.js file

--- members-only/routes/routes.js
+++ members-only/routes/routes.js
@@ -4,6 +4,7 @@ const { pgPool } = require('../db/pool');
 const bcrypt = require('bcryptjs');
 // const { v4: uuidv4 } = require('uuid');
 const { body, validationResult } = require('express-validator');
+const passport = require('passport');
 
 // firstname, lastname, username, password
 
@@ -11,10 +12,26 @@ const alphaErr = 'must contain only letters.';
 const lengthErr = 'must be between 1 and 25 characters';
 
 const validateUser = [
-  body('firstname').trim().isAlpha().withMessage(`First name ${alphaErr}`).isLength({ min: 1, max: 25 }).withMessage(`First name ${lengthErr}`),
-  body('lastname').trim().isAlpha().withMessage(`Last name ${alphaErr}`).isLength({ min: 1, max: 25 }).withMessage(`Last name ${lengthErr}`),
-  body('username').trim().isLength({ min: 1, max: 25 }).withMessage(`Username ${lengthErr}`),
-  body('password').trim().isLength({ min: 1, max: 25 }).withMessage(`Password ${lengthErr}`),
+  body('firstname')
+    .trim()
+    .isAlpha()
+    .withMessage(`First name ${alphaErr}`)
+    .isLength({ min: 1, max: 25 })
+    .withMessage(`First name ${lengthErr}`),
+  body('lastname')
+    .trim()
+    .isAlpha()
+    .withMessage(`Last name ${alphaErr}`)
+    .isLength({ min: 1, max: 25 })
+    .withMessage(`Last name ${lengthErr}`),
+  body('username')
+    .trim()
+    .isLength({ min: 1, max: 25 })
+    .withMessage(`Username ${lengthErr}`),
+  body('password')
+    .trim()
+    .isLength({ min: 1, max: 25 })
+    .withMessage(`Password ${lengthErr}`),
   body('passwordConfirmation').custom((value, { req }) => {
     if (value !== req.body.password) {
       throw new Error('Passwords do not match');
@@ -43,7 +60,10 @@ indexRouter.post('/register', [
         });
       }
 
-      const existingUser = await pgPool.query('SELECT * FROM members WHERE username = $1', [username]);
+      const existingUser = await pgPool.query(
+        'SELECT * FROM members WHERE username = $1',
+        [username]
+      );
 
       if (existingUser.rows.length > 0) {
         return res.status(400).render('register', {
@@ -53,15 +73,45 @@ indexRouter.post('/register', [
       }
 
       const hashedPassword = await bcrypt.hash(password, 12);
-      await pgPool.query('INSERT INTO members (fullname, username, password, membership_status, isadmin) VALUES ($1, $2, $3, $4, $5)', [`${firstname} ${lastname}`, username, hashedPassword, 'active', false]);
+      await pgPool.query(
+        'INSERT INTO members (fullname, username, password, membership_status, isadmin) VALUES ($1, $2, $3, $4, $5)',
+        [`${firstname} ${lastname}`, username, hashedPassword, 'active', false]
+      );
     } catch (err) {
       console.log(err);
       return next(err);
     }
     res.render('confirmMembership', {
       title: 'Confirm Membership',
+      username: username,
     });
   },
 ]);
+
+indexRouter.post(
+  '/authenticate',
+  passport.authenticate('local', {
+    successRedirect: '/confirmMembership',
+    failureRedirect: '/register',
+  })
+);
+
+indexRouter.post('/confirmMembership', async (req, res) => {
+  console.log(req.body);
+
+  // if (membershipCode === process.env.MEMBERSHIP_CODE) {
+  //   await pgPool.query('SELECT * FROM members WHERE members.username = $1', [
+  //     username,
+  //   ]);
+  // }
+  res.end('Confirmed');
+});
+
+/***** LOGIN ******/
+indexRouter.get('/login', (req, res) => {
+  res.render('index', {
+    user: req.user,
+  });
+});
 
 module.exports = indexRouter;
