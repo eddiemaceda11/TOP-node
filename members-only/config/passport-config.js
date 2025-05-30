3e8d1d2a7830020c025011f81b7140fa3f572bcc
Eddie Maceda <eddiemaceda11@gmail.com>
2025-05-29 21:43:44 -0700

    Refactor the passport configuration

--- members-only/config/passport-config.js
+++ members-only/config/passport-config.js
@@ -1,15 +1,25 @@
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 const { pgPool } = require('../db/pool');
+const bcrypt = require('bcryptjs');
 
 passport.use(
   new LocalStrategy(async (username, password, done) => {
     try {
-      const { rows } = await pgPool.query('SELECT * FROM users WHERE username = $1', [username]);
+      const { rows } = await pgPool.query(
+        'SELECT * FROM members WHERE username = $1',
+        [username]
+      );
       const user = rows[0];
       console.log('user: ', user);
+
       if (!user) {
         return done(null, false, { message: 'Incorrect username' });
+      }
+
+      const match = await bcrypt.compare(password, user.password);
+      if (!match) {
+        return done(null, false, { message: 'Incorrect password' });
       }
       return done(null, user);
     } catch (err) {
@@ -25,7 +35,9 @@ passport.serializeUser((user, done) => {
 
 passport.deserializeUser(async (userId, done) => {
   try {
-    const { rows } = await pgPool.query('SELECT * FROM users WHERE id = $1', [userId]);
+    const { rows } = await pgPool.query('SELECT * FROM members WHERE id = $1', [
+      userId,
+    ]);
     const user = rows[0];
     done(null, user);
   } catch (err) {
