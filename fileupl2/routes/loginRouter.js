88a496ef789d3edd550a0c62506e4055fb7f1ffe
Eddie Maceda <eddiemaceda11@gmail.com>
2025-06-11 20:21:55 -0700

    Update the loginRouter

--- fileupl2/routes/loginRouter.js
+++ fileupl2/routes/loginRouter.js
@@ -3,9 +3,49 @@ const passport = require('passport');
 
 const loginRouter = Router();
 
+const directory = {
+  name: 'root',
+  isOpen: true,
+  files: [{ name: 'root.txt' }, { name: 'readme.md' }],
+  folders: [
+    {
+      name: 'subfolder1',
+      isOpen: false,
+      files: [{ name: 'hidden.txt' }],
+      folders: [],
+    },
+    {
+      name: 'subfolder2',
+      isOpen: true,
+      files: [{ name: 'sub2file1.txt' }, { name: 'sub2file2.log' }],
+      folders: [
+        {
+          name: 'nestedFolder1',
+          isOpen: true,
+          files: [{ name: 'deepfile.md' }],
+          folders: [],
+        },
+        {
+          name: 'nestedFolder2',
+          isOpen: false,
+          files: [{ name: 'hiddenDeep.txt' }],
+          folders: [],
+        },
+      ],
+    },
+    {
+      name: 'subfolder3',
+      isOpen: true,
+      files: [],
+      folders: [],
+    },
+  ],
+};
+
 loginRouter.get('/', (req, res) => {
   res.render('login', {
     user: req.user,
+    directory,
   });
 });
 
